648dd177233aeb537cbb933cea3515136b6908a1
Candice <wuqiong910718@163.com>
2020-03-22 21:05:50 +0800

    创建单例组件Notice

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 import Bus from './utils/Bus.js'
+import create from './utils/create.js';
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Bus();
+Vue.prototype.$create = create;
 
+// new Vue结果是根实例，$root vm
+// App是根组件 $children[0]
 new Vue({
   render: h => h(App),
 }).$mount('#app')
